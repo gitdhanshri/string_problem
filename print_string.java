862b13fc4c72fdfacc02626f2a51741516b501ee
gitdhanshri <2022bit049@sggs.ac.in>
2024-02-08 21:29:26 +0530

    all functionalities on Strings

---
+++ print_string.java
@@ -0,0 +1,56 @@
+class Print{
+public static void main(String[] args){
+               // String str="DhanshriVitthalraoBaswade";
+                String str="Dhanshri@2022bit049";
+               // String str="Dhanshri@2022bit049";
+                System.out.println(str);
+                int count[]=new int [10];
+	/*	for(int i=0;i<str.length();i++){
+		         System.out.println(count[str.charAt(i)-'a']++);
+		}	
+	
+	System.out.println(str.length());
+	System.out.println(str.charAt(3));
+	System.out.println(str.substring(4));
+	System.out.println(str.substring(2,5));
+	String s1="Dhanshri";
+	String s2="Dhanshri";
+	System.out.println(s1);
+	System.out.println(s2);
+	if(s1==s2){
+		System.out.println("\n1-->yes");
+	}
+	else{
+		System.out.println("\n2-->no");
+	}
+	String s3=new String("geeks");
+		if(s1==s3){
+		System.out.println("\n2-->yes");
+	}
+	else{
+		System.out.println("\n2-->no");
+	}
+		String s1="Dhanshri";
+		String s2="DhanshriVitthalrao";
+		int res=s1.compareTo(s2);
+		if(res==0){
+		System.out.println("Same");
+		}
+		if(res>0){
+		System.out.println("s1 is greater");
+		}
+		
+		if(res<0){
+		System.out.println("S1 is smaller");
+		}
+		System.out.println(s2.indexOf(s1));*/
+		String s1="shriVitthalrao";
+		String s2="shri";
+		s1=s1+"inSGGS"; 
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1==s2);
+			
+}
+}
+
