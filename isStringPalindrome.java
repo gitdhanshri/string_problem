72b7dcd889e1e13f93767b54374b6edee107dcd7
gitdhanshri <2022bit049@sggs.ac.in>
2024-03-30 23:03:55 +0530

    Modification in code isPalindromestring.java file

--- isStringPalindrome.java
+++ isStringPalindrome.java
@@ -45,8 +45,21 @@ class PalindromeString{
 
 		return (s1.equals(str));	
 	}
+	
+	boolean approach3(String s1){
+		String rev="";
+		char[] c=s1.toCharArray();
+		for(int i=s1.length()-1;i>=0;i--){
+			 rev+=c[i];
+		}
+		if(s1.equals(rev)){
+				return true;
+		}
+		
+		return false;
+	}
 	String[] arryContainerThatStoreRandomGeneratedString(){
-		long size=500000;
+		long size=100000;
 		String arr[]=new String[(int)size];
 			for(int i=0;i<size;i++){
 				arr[i]=generateRandomOneString();
@@ -81,6 +94,20 @@ class PalindromeString{
 		}
 		return  totalPalString;
 	}
+	long count_palindromeAndNonPalString_By_M3(String[] str){
+		int i=0;
+		long totalPalString=0;
+		long totalNonPalString=0;
+		for(i=0;i<str.length;i++){
+			if(approach3(str[i])){
+				totalPalString++;
+			}
+			else{
+				totalNonPalString++;
+			}
+		}
+		return  totalPalString;
+	}
 	String[] storePalindromeStringInStringArray_M1(String[] str,long size){
 		 int k=0;
 		String []s=new String[(int)size];
@@ -103,7 +130,7 @@ class PalindromeString{
 		 int k=0;
 		String []s=new String[(int)size];
 		for(int i=0;i<str.length;i++){
-			if(approach1(str[i])==true){
+			if(approach2(str[i])==true){
 				s[k]=str[i];
 				k++;
 				if(k==size){
@@ -137,8 +164,9 @@ class PalindromeString{
 			long startTime, endTime;
 			long arr1[]=new long[100];
 			long arr2[]=new long[100];
+			long arr3[]=new long[100];
 			String[] str=s1.arryContainerThatStoreRandomGeneratedString();
-			long i = 0,numberofTimes=8;
+			long i = 0,numberofTimes=10;
 		while(i<1){
 				for(int k=0;k<numberofTimes;k++){
 					startTime=System.nanoTime();
@@ -148,18 +176,24 @@ class PalindromeString{
 				}
 				for(int k=0;k<numberofTimes;k++){
 					startTime=System.nanoTime();
-					s1.count_palindromeAndNonPalString_By_M1(str);
+					s1.count_palindromeAndNonPalString_By_M2(str);
 					endTime =System.nanoTime();
 					arr2[k]=endTime-startTime;
+				}
+				for(int k=0;k<numberofTimes;k++){
+					startTime=System.nanoTime();
+					s1.count_palindromeAndNonPalString_By_M3(str);
+					endTime =System.nanoTime();
+					arr3[k]=endTime-startTime;
 				}
 				i++;
 			}
 			long time1=s1.findAverageTimeOf(arr1);
 			long time2=s1.findAverageTimeOf(arr2);
+			long time3=s1.findAverageTimeOf(arr3);
 			System.out.println("Time Taken for 1st Approach-->"+time1);
 			System.out.println("Time Taken for 2nd Approach-->"+time2);
-			
-		
+			System.out.println("Time Taken for 3rd Approach-->"+time3);
 	}
 }	
 
